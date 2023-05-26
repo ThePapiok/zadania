932f0339a40fa75efce8171f1fb32aabdbd3901e
ThePapiok <132700662+ThePapiok@users.noreply.github.com>
2023-05-26 22:58:22 +0200

    Delete HTML/stronainternetowa directory

--- HTML/stronainternetowa/funkcje.php
+++
@@ -1,9 +0,0 @@
-<?php
-function pisz($wiadomosc)
-{
-	$plik = fopen("chat.txt", "a");
-	fwrite($plik, $wiadomosc);
-	fwrite($plik, PHP_EOL);
-	fclose($plik);
-}
-?>
--- HTML/stronainternetowa/s.php
+++
@@ -1,9 +0,0 @@
-<?php   
-    include './funkcje.php';
-    switch($_POST["functionname"]){ 
-
-        case 'funkcja': 
-            pisz($_POST['arguments'][0]);
-            break;      
-    }   
-?>
